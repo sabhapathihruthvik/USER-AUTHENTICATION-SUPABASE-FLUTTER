af0688f6cedffa2452fdfaf07718fdbf0253b21b
SABHAPATHI HRUTHVIK <129086803+sabhapathihruthvik@users.noreply.github.com>
2024-12-24 12:41:46 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: "https://xgvmzhkwufqwtithlxmp.supabase.co",
+    url: "",
     anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhndm16aGt3dWZxd3RpdGhseG1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5Njk0OTMsImV4cCI6MjA1MDU0NTQ5M30.-9Lb6gvDzELLVvUUW62G1p4Kip7RHc00dHcun6ybI98",
+        "",
   );
   runApp(const MyApp());
 }
